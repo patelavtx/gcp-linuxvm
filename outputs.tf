9e66990b81cccbcc96975be2dadb900d7e233581
patelavtx <apatel@aviatrix.com>
2023-06-26 12:09:27 +0100

    refine outputs

--- outputs.tf
+++ outputs.tf
@@ -2,7 +2,8 @@ output "vm_name" {
   description = "VM Name"
   value = google_compute_instance.vm_instance_public.name
 }
-output "vm" {
+output "vm_pubicip" {
   description = "Public IP"
-  value = google_compute_instance.vm_instance_public
+  value = google_compute_instance.vm_instance_public.network_interface[0].access_config[0].nat_ip
 }
+
