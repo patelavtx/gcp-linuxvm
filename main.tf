dc7359843d9352b65a53ed08cba5b762993bbfe2
patelavtx <apatel@aviatrix.com>
2023-06-26 11:31:04 +0100

    update index.html

--- main.tf
+++ main.tf
@@ -7,6 +7,7 @@ data "template_file" "linux-metadata" {
 template = <<EOF
 sudo apt-get update; 
 sudo apt-get install -y apache2;
+echo "<h1>${var.vm_name}</h1>" | sudo tee /var/www/html/index.html
 sudo systemctl start apache2;
 sudo systemctl enable apache2;
 EOF
